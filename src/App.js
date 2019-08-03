3964a61d271c39ba6ce9fbe9db790346a5f07025
Gawain B. II <1490181+gawainb@users.noreply.github.com>
2019-08-02 21:36:16 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import withRoot from './modules/withRoot';
+// --- Post bootstrap -----
+import React from 'react';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+import ProductCategories from './modules/views/ProductCategories';
+import ProductSmokingHero from './modules/views/ProductSmokingHero';
+import AppFooter from './modules/views/AppFooter';
+import ProductHero from './modules/views/ProductHero';
+import ProductValues from './modules/views/ProductValues';
+import ProductHowItWorks from './modules/views/ProductHowItWorks';
+import ProductCTA from './modules/views/ProductCTA';
+import AppAppBar from './modules/views/AppAppBar';
+
+Amplify.configure(awsconfig);
+
+function App() {
+  return (
+    <React.Fragment>
+      <AppAppBar />
+      <ProductHero />
+      <ProductValues />
+      <ProductCategories />
+      <ProductHowItWorks />
+      <ProductCTA />
+      <ProductSmokingHero />
+      <AppFooter />
+    </React.Fragment>
+  );
+}
+
+export default withAuthenticator(withRoot(App), true);
